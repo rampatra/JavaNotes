0636b9e15fd5c903f18705a66e416af22df17408
Ram swaroop <ramswaroop@bsb.in>
2015-05-24 11:13:33 +0530

    share buttons added

--- js/main.js
+++ js/main.js
@@ -1,22 +1,40 @@
 var elements = document.getElementsByTagName('script')
 
-Array.prototype.forEach.call(elements, function(element) {
-  if (element.type.indexOf('math/tex') != -1) {
-     // Extract math markdown
-     var textToRender = element.innerText || element.textContent;
+Array.prototype.forEach.call(elements, function (element) {
+    if (element.type.indexOf('math/tex') != -1) {
+        // Extract math markdown
+        var textToRender = element.innerText || element.textContent;
 
-     // Create span for KaTeX
-     var katexElement = document.createElement('span');
+        // Create span for KaTeX
+        var katexElement = document.createElement('span');
 
-     // Support inline and display math
-     if (element.type.indexOf('mode=display') != -1){
-       katexElement.className += "math-display";
-       textToRender = '\\displaystyle {' + textToRender + '}';
-     } else {
-       katexElement.className += "math-inline";
-     }
+        // Support inline and display math
+        if (element.type.indexOf('mode=display') != -1) {
+            katexElement.className += "math-display";
+            textToRender = '\\displaystyle {' + textToRender + '}';
+        } else {
+            katexElement.className += "math-inline";
+        }
 
-     katex.render(textToRender, katexElement);
-     element.parentNode.insertBefore(katexElement, element);
-  }
+        katex.render(textToRender, katexElement);
+        element.parentNode.insertBefore(katexElement, element);
+    }
 });
+
+// Share bar
+var share_bar = document.getElementsByClassName('share-bar');
+
+for (var i = 0; i < share_bar.length; i++) {
+
+    var html = '<iframe allowtransparency="true" frameborder="0" scrolling="no"' +
+        'src="http://platform.twitter.com/widgets/tweet_button.html?url=' + encodeURIComponent(window.location) + '&amp;text=' + encodeURIComponent(document.title) + '&amp;via=ramswarooppatra&amp;hashtags=java-concepts&amp;count=horizontal"' +
+        'style="width:105px; height:21px;">' +
+        '</iframe>' +
+
+        '<iframe src="//www.facebook.com/plugins/like.php?href=' + encodeURIComponent(window.location) + '&amp;width&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;share=true&amp;height=21&amp;appId=217347368312341&amp;width=150" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:150px; height:21px;" allowTransparency="true"></iframe>' +
+
+        '<script src="https://apis.google.com/js/plusone.js" async></script> <g:plusone href="' + encodeURIComponent(window.location) + '" size="medium"></g:plusone>';
+
+    share_bar[i].innerHTML = html;
+    share_bar[i].style.display = 'inline-block';
+}
